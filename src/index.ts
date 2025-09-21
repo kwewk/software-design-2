24d4588c334ce70c146ede2b1bb4e0a7486c0362
kwewk <egor.a.gavrilin@gmail.com>
2025-09-21 15:19:44 +0200

    feat!: change add signature to accept number[]

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { config } from './config.js';
 export { config } from './config.js';
 
-export function add(a: number, b: number): number {
-  return a + b;
+export function add(values: number[]): number {
+  return values.reduce((acc, x) => acc + x, 0);
 }
 
 export function capitalize(s: string): string {
